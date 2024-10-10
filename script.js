c830d4e547bf950c56edb9d4b1ba3ccb090344c9
igorkokot <100727427+igorkokot@users.noreply.github.com>
2024-10-10 02:20:30 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("toggle-button").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    document.body.classList.toggle("light-mode");
+});
