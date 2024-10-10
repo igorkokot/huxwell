9cd62d4020dc5a4ddacc198d56e895ed7851b06f
igorkokot <100727427+igorkokot@users.noreply.github.com>
2024-10-11 01:16:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
 document.getElementById("toggle-slider").addEventListener("click", function() {
     const body = document.body;
     const sliderCircle = document.querySelector(".slider-circle");
-    const image = document.getElementById("mode-image");
 
     body.classList.toggle("dark-mode");
     body.classList.toggle("light-mode");
 
     if (body.classList.contains("dark-mode")) {
         sliderCircle.textContent = "🌑";
-        sliderCircle.style.transform = "translateX(30px)";
-        image.src = "Logo_Tryb_Ciemny.jpg";
+        sliderCircle.style.transform = "translateX(20px)";
     } else {
         sliderCircle.textContent = "☀️";
         sliderCircle.style.transform = "translateX(0px)";
-        image.src = "Logo_Tryb_Jasny.jpg";
-    }
-
-    if (window.innerWidth <= 1080) {
-        image.style.display = 'none';
-    } else {
-        image.style.display = 'block';
-    }
-});
-
-window.addEventListener('resize', function() {
-    const image = document.getElementById("mode-image");
-    if (window.innerWidth <= 1080) {
-        image.style.display = 'none';
-    } else {
-        image.style.display = 'block';
     }
 });
