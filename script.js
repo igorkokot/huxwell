5c585c341270331d51d059b71636dabe3a1f38a5
igorkokot <100727427+igorkokot@users.noreply.github.com>
2024-10-10 21:09:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,19 @@ document.getElementById("toggle-slider").addEventListener("click", function() {
     const sliderCircle = document.querySelector(".slider-circle");
     const image = document.getElementById("mode-image");
 
-    // Zmiana trybu
     body.classList.toggle("dark-mode");
     body.classList.toggle("light-mode");
 
-    // Aktualizacja emoji i pozycji kółka
     if (body.classList.contains("dark-mode")) {
-        sliderCircle.textContent = "🌑"; // Emoji dla trybu ciemnego
-        sliderCircle.style.transform = "translateX(30px)"; // Przesunięcie kółka na prawą stronę
-        image.src = "Logo_Tryb_Ciemny.jpg"; // Zmiana obrazka na tryb ciemny
+        sliderCircle.textContent = "🌑";
+        sliderCircle.style.transform = "translateX(30px)";
+        image.src = "Logo_Tryb_Ciemny.jpg";
     } else {
-        sliderCircle.textContent = "☀️"; // Emoji dla trybu jasnego
-        sliderCircle.style.transform = "translateX(0px)"; // Przesunięcie kółka na lewą stronę
-        image.src = "Logo_Tryb_Jasny.jpg"; // Zmiana obrazka na tryb jasny
+        sliderCircle.textContent = "☀️";
+        sliderCircle.style.transform = "translateX(0px)";
+        image.src = "Logo_Tryb_Jasny.jpg";
     }
 
-    // Ukrywanie grafiki w trybie pionowym
-    const container = document.querySelector('.container');
     if (window.innerWidth <= 1080) {
         image.style.display = 'none';
     } else {
@@ -27,7 +23,6 @@ document.getElementById("toggle-slider").addEventListener("click", function() {
     }
 });
 
-// Sprawdzanie rozmiaru okna i aktualizacja widoczności grafiki
 window.addEventListener('resize', function() {
     const image = document.getElementById("mode-image");
     if (window.innerWidth <= 1080) {
